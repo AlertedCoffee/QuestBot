463f0e6f490d5f701e5e023da245511545816c6d
AlertedCoffee <ozolin.matvei@yandex.ru>
2023-11-19 08:02:45 +0300

    new vers for StationsFactory

--- Model/StationsFactory.py
+++ Model/StationsFactory.py
@@ -2,14 +2,39 @@ from Model.StationModel import StationCard
 
 
 class StationsFactory:
-    cards = []
+    cards_first_group = []
+    cards_second_group = []
+    cards_third_group = []
 
-    stations = ['Актовый зал', 'Холл второго этажа', 'Холл третьего этажа', 'Пианино на четвертом этаже',
+    stations_1 = ['Кто проживает на дне океана', 'Холл второго этажа', 'Холл третьего этажа', 'Пианино на четвертом этаже',
                 'Большой спортивный зал', 'Библиотека', 'Холл перед столовой', 'Лабиринт шкафчиков']
-    answers = ['сорочинцы', 'Потемкин', 'Яновский', 'Ревизор',
+    answers_1 = ['Спанч Боб', 'Потемкин', 'Яновский', 'Ревизор',
                'Алов', 'месяц', 'Пушкин', 'голова']
 
-    def get_cards(self) -> list[StationCard]:
-        for i, elem in enumerate(self.stations):
-            self.cards.append(StationCard(station_name=elem, answer=self.answers[i]))
-        return self.cards
+    stations_2 = ['Кто проживает на дне океана', 'Холл второго этажа', 'Холл третьего этажа', 'Пианино на четвертом этаже',
+                  'Большой спортивный зал', 'Библиотека', 'Холл перед столовой', 'Лабиринт шкафчиков']
+    answers_2 = ['Спанч Боб', 'Потемкин', 'Яновский', 'Ревизор',
+                 'Алов', 'месяц', 'Пушкин', 'голова']
+
+    stations_3 = ['Кто проживает на дне океана', 'Холл второго этажа', 'Холл третьего этажа', 'Пианино на четвертом этаже',
+                  'Большой спортивный зал', 'Библиотека', 'Холл перед столовой', 'Лабиринт шкафчиков']
+    answers_3 = ['Спанч Боб', 'Потемкин', 'Яновский', 'Ревизор',
+                 'Алов', 'месяц', 'Пушкин', 'голова']
+
+    def get_cards_first_group(self) -> list[StationCard]:
+        if not self.cards_first_group:
+            for i, elem in enumerate(self.stations_1):
+                self.cards_first_group.append(StationCard(station_name=elem, answer=self.answers_1[i]))
+        return self.cards_first_group
+
+    def get_cards_second_group(self) -> list[StationCard]:
+        if not self.cards_second_group:
+            for i, elem in enumerate(self.stations_2):
+                self.cards_second_group.append(StationCard(station_name=elem, answer=self.answers_2[i]))
+        return self.cards_second_group
+
+    def get_cards_third_group(self) -> list[StationCard]:
+        if not self.cards_third_group:
+            for i, elem in enumerate(self.stations_3):
+                self.cards_third_group.append(StationCard(station_name=elem, answer=self.answers_3[i]))
+        return self.cards_third_group
